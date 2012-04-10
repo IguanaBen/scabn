015eb71eb69586d4ada9f0f236b7264817da789f
bluey80 <bluey80@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-10 19:22:19 +0000

    Reference new icon.
    git-svn-id: http://plugins.svn.wordpress.org/simple-cart-buy-now/trunk@529575 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/js/tinymce/editor_plugin.js
+++ includes/js/tinymce/editor_plugin.js
@@ -4,5 +4,5 @@
 		{init:function(ed,url)
 			{ed.addCommand('mcescabn',function()
 				{ed.windowManager.open({file:url+'/dialog.php',width:420+parseInt(ed.getLang('scabn.delta_width',0)),height:500+parseInt(ed.getLang('scabn.delta_height',0)),inline:1},{plugin_url:url,some_custom_arg:'custom arg'})});
-				ed.addButton('scabn',{title:'Add SCABN Item or Checkout',cmd:'mcescabn',image:url+'/img/wp-chkt.png'});ed.onNodeChange.add(function(ed,cm,n){cm.setActive('scabn',n.nodeName=='IMG')})},
+				ed.addButton('scabn',{title:'Add SCABN Item or Checkout',cmd:'mcescabn',image:url+'/img/scabn.png'});ed.onNodeChange.add(function(ed,cm,n){cm.setActive('scabn',n.nodeName=='IMG')})},
 				createControl:function(n,cm){return null},getInfo:function(){return{longname:'scabn plugin',author:'Some author',authorurl:'http://tinymce.moxiecode.com',infourl:'http://wiki.moxiecode.com/index.php/TinyMCE:Plugins/scabn',version:"1.0"}}});tinymce.PluginManager.add('scabn',tinymce.plugins.scabnPlugin)})();
