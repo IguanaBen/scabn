16532f188d7287df8b854f02e90afe5185683091
bluey80 <bluey80@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-09 03:54:40 +0000

    Moved smoe functions into Backend class
    git-svn-id: http://plugins.svn.wordpress.org/simple-cart-buy-now/branches/scabn-2.0@622821 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- backend.php
+++ backend.php
@@ -24,12 +24,69 @@ class scabn_Backend {
 		session_start();      // start the session
 		$cart =& $_SESSION['wfcart']; // load the cart from the session
 		if(!is_object($cart)) $cart = new wfCart(); // if there isn't a cart, create a new (empty) one	
-		scabn_request();
+		scabn_Backend::scabn_request();
 
 	
 
 }
 
+	function scabn_request(){
+		//This function handles all the client input to change cart via GET / POST requests.
+		//Probably a good place to sanitize the data.
+	
+		$cart =& $_SESSION['wfcart']; // get the cart
+
+		if (isset($_REQUEST['action']) && $_REQUEST['action'] == 'add_item'  ){
+		
+
+			if ( isset($_REQUEST['item_options']) && (  $_REQUEST['item_options'] != '')  ) {
+				//item options set -- check if it is a list of options with ':' as separator			        		
+	        	$temp=explode(':',$_REQUEST['item_options']);
+	        	//if it is, it should be formatted as 'optionname:price' 
+				if ( count($temp) == 2) {
+					$price=floatval($temp[1]);
+					$itemoptionvalue=sanitize_text_field($temp[0]);
+	
+				} else {
+					$price=floatval($_REQUEST['item_price']);
+					$itemoptionvalue=sanitize_text_field($_REQUEST['item_options']);
+				}
+	
+				$item_options = array (sanitize_title($_REQUEST['item_options_name']) => $itemoptionvalue);
+				$item_id = sanitize_title($_REQUEST['item_id']."-".$itemoptionvalue);
+	
+			} else {
+				$item_options = array ();
+				$price = floatval($_REQUEST['item_price']);
+				$item_id = sanitize_title($_REQUEST['item_id']);
+			}		
+			$cart->add_item($item_id,intval($_REQUEST['item_qty']),$price,sanitize_text_field($_REQUEST['item_name']),$item_options,esc_url($_REQUEST['item_url']),floatval($_REQUEST['item_weight']));		
+		}
+	
+		if (isset ($_REQUEST['remove']) && $_REQUEST['remove'] ){
+		   $cart->del_item(sanitize_title($_REQUEST['remove']));
+		}
+	
+		if (isset($_REQUEST['empty']) && $_REQUEST['empty']  ){
+		   $cart->empty_cart();
+		}
+	
+		if (isset($_REQUEST['update']) && $_REQUEST['update']  ){				
+			for ($i=0; $i<sizeof($cart->items); $i++){
+				if (ctype_digit($_POST['qty_'.$i])){												
+					$cart->edit_item(sanitize_title($_POST['item_'.$i]),intval($_POST['qty_'.$i]));
+			   	}
+			}
+		}
+	
+		if (isset($_REQUEST['update_item']) && $_REQUEST['update_item']  ){
+		   if (ctype_digit($_REQUEST['qty'])){
+		   	$cart->edit_item(sanitize_title($_REQUEST['id']),intval($_REQUEST['qty']));
+		   }
+		}
+	
+	
+	}
 
 
 
