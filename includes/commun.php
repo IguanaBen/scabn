e4040b0dae81eb1fa0a8ebea201af56f3d15e64d
bluey80 <bluey80@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-09 03:49:44 +0000

    Added same sanitization to input data
    git-svn-id: http://plugins.svn.wordpress.org/simple-cart-buy-now/branches/scabn-2.0@622820 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/commun.php
+++ includes/commun.php
@@ -7,41 +7,39 @@
 
 function scabn_request(){
 	//This function handles all the IO via GET / POST requests.
-	//This is where we need to sanitize incoming information.
-	//echo "Testing functions: <BR>";
-	//echo "Result from call is: ". Scabn_Backend::getItemPricing('serial',5,32) . ".<BR>";
-	//echo "Result from call is: ". apply_filters(getItemPricing,'serial',5,32) . ".<BR>";
-
-
-	$cart =& $_SESSION['wfcart'];
+	//Probably a good place to sanitize the data.
+	
+	$cart =& $_SESSION['wfcart']; // get the cart
 
 	if (isset($_REQUEST['action']) && $_REQUEST['action'] == 'add_item'  ){
 		
 
-		if ( isset($_REQUEST['item_options']) && ( ( $_REQUEST['item_options'] != 'undefined') && ( $_REQUEST['item_options'] != '') ) ) {
-        		$temp=explode(':',$_REQUEST['item_options']);
+		if ( isset($_REQUEST['item_options']) && (  $_REQUEST['item_options'] != '')  ) {
+			//item options set -- check if it is a list of options with ':' as separator			        		
+        	$temp=explode(':',$_REQUEST['item_options']);
+        	//if it is, it should be formatted as 'optionname:price' 
 			if ( count($temp) == 2) {
-				$price=$temp[1];
-				$itemoptionname=$temp[0];
+				$price=floatval($temp[1]);
+				$itemoptionvalue=sanitize_text_field($temp[0]);
 
 			} else {
-				$price=$_REQUEST['item_price'];
-				$itemoptionname=$_REQUEST['item_options'];
+				$price=floatval($_REQUEST['item_price']);
+				$itemoptionvalue=sanitize_text_field($_REQUEST['item_options']);
 			}
 
-			$item_options = array ($_REQUEST['item_options_name'] => $itemoptionname);
-			$item_id = sanitize_title($_REQUEST['item_id']."-".$itemoptionname);
+			$item_options = array (sanitize_title($_REQUEST['item_options_name']) => $itemoptionvalue);
+			$item_id = sanitize_title($_REQUEST['item_id']."-".$itemoptionvalue);
 
 		} else {
 			$item_options = array ();
-			$price = $_REQUEST['item_price'];
+			$price = floatval($_REQUEST['item_price']);
 			$item_id = sanitize_title($_REQUEST['item_id']);
 		}		
-		$cart->add_item($item_id,$_REQUEST['item_qty'],$price,$_REQUEST['item_name'],$item_options,$_REQUEST['item_url'],$_REQUEST['item_weight']);		
+		$cart->add_item($item_id,intval($_REQUEST['item_qty']),$price,sanitize_text_field($_REQUEST['item_name']),$item_options,esc_url($_REQUEST['item_url']),floatval($_REQUEST['item_weight']));		
 	}
 
 	if (isset ($_REQUEST['remove']) && $_REQUEST['remove'] ){
-	   $cart->del_item($_REQUEST['remove']);
+	   $cart->del_item(sanitize_title($_REQUEST['remove']));
 	}
 
 	if (isset($_REQUEST['empty']) && $_REQUEST['empty']  ){
@@ -50,16 +48,15 @@ function scabn_request(){
 
 	if (isset($_REQUEST['update']) && $_REQUEST['update']  ){				
 		for ($i=0; $i<sizeof($cart->items); $i++){
-			if (ctype_digit($_POST['qty_'.$i])){
-				//echo is_int($_POST['qty_'.$i]);								
-				$cart->edit_item($_POST['item_'.$i],$_POST['qty_'.$i]);
+			if (ctype_digit($_POST['qty_'.$i])){												
+				$cart->edit_item(sanitize_title($_POST['item_'.$i]),intval($_POST['qty_'.$i]));
 		   	}
 		}
 	}
 
 	if (isset($_REQUEST['update_item']) && $_REQUEST['update_item']  ){
 	   if (ctype_digit($_REQUEST['qty'])){
-	   	$cart->edit_item($_REQUEST['id'],$_REQUEST['qty']);
+	   	$cart->edit_item(sanitize_title($_REQUEST['id']),intval($_REQUEST['qty']));
 	   }
 	}
 
