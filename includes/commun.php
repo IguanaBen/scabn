9dcc5a8586f089fd59190e14f511b1138cbfa019
bluey80 <bluey80@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-10 17:23:40 +0000

    Removed all functions from here to better places
    git-svn-id: http://plugins.svn.wordpress.org/simple-cart-buy-now/branches/scabn-2.0@623409 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/commun.php
+++ includes/commun.php
@@ -8,62 +8,11 @@
 
 
 
-/**
- * Return Items Options Pair
- */
-
-function scabn_item_options ($options_arr){
-
-	foreach($options_arr as $key=>$value){
-
-	   $options_pair = $key.": ".$value."<br />";
-
-	}
-
-	return $options_pair;
-
-}
-
-/* Simple options in - format */
-function scabn_item_options_old ($options_arr,$separator="<br/>"){
-	$begin=TRUE;
-	if ( count($options_arr) == 1 )	{
-   	foreach($options_arr as $key=>$value) {
-			$options_pair = $value;
-      }
-	} else {
-   	foreach($options_arr as $key=>$value) {
-	   	$options_pair = $key."-".$value;
-	      if ($begin == FALSE) {
-	      	$options_pair .= $separator;
-	      } else {
-		   	$begin=FALSE;
-	   	}
-   	}
-	}
-	return $options_pair;
-
-}
-
-
-
 
 /**
  *  Currency code & symbol
  */
 
-
-function scabn_curr_symbol($code){	
-
-    $scabn_currency_codes= scabn_Backend::getCurrencies();
-	
-	$d = $scabn_currency_codes[$code][0];
-	
-	$symbol = "&#".$d.";";
-	
-	return $symbol;
-
-}
 
 
 
